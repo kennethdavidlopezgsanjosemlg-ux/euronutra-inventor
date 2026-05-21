1134675aec1fc51b16dc2ca1625ad06cce12569b
kennethdavidlopezgsanjosemlg-ux <kennethdavilopezg.sanjosemlg@fundacionloyola.net>
2026-05-21 10:13:55 +0200

    commit cambiso base de datos

--- database/db.js
+++ database/db.js
@@ -6,16 +6,19 @@ const conexion = mysql.createConnection({host: process.env.MYSQLHOST,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT
-    console.log("DEBUG VARIABLES:", process.env.MYSQLHOST, process.env.MYSQLPORT);
+    
 });
 
 // Conectar a la base de datos
 conexion.connect((err) => {
     if (err) {
         console.error('Error al conectar a la base de datos:', err);
+        console.error("DEBUG VARIABLES:", process.env.MYSQLHOST, process.env.MYSQLPORT);
         return;
     }
     console.log('Conexión exitosa');
+    console.error("DEBUG VARIABLES:", process.env.MYSQLHOST, process.env.MYSQLPORT);
+
 });
 
 module.exports = conexion;
