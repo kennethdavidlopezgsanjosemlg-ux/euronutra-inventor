9361910752de839f4c9eb1e1665488335921777a
kennethdavidlopezgsanjosemlg-ux <kennethdavilopezg.sanjosemlg@fundacionloyola.net>
2026-05-21 12:07:18 +0200

    commit cambios

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escuchando en puerto: ${PORT}`);
-  console.log(`creado por kenneth`);
+
 });
   
